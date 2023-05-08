507ab3022007f76bd6a8cf6404f4379c181bbd13
hdjs <johan.svartdal@husbanken.no>
2023-05-08 09:56:42 +0200

    Initial commit

---
+++ installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# make sure the basrc file exists
+if [ ! -f ".bashrc" ]; then
+  echo "The file '.bashrc' does not exist in the current directory."
+  exit 1
+fi
+
+# make sure .bashrcextended is installed
+if ! grep -q ".bashrcextended" .bashrc; then
+  echo "Bashrcextended is not installed. Installing..."
+  echo "" >> .bashrc
+  echo "# Source global definitions" >> .bashrc
+  echo "if [ -f ./bashrcextended ]; then" >> .bashrc
+  echo "  . ./bashrcextended" >> .bashrc
+  echo "fi" >> .bashrc
+  echo "Bashrcextended installed!"
+fi
+
+# download the bashrcextended file
+if ! curl -s -o .bashrcextended "https://www.mywebsite.com/.bashrcextended"; then
+    echo "Error updating Bashrcextended"
+    exit 1
+fi
+echo "Bashrcextended updated!"
